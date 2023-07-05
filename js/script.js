8dde3f1034024784cefe1f56cb428b5841029463
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-07-06 01:21:13 +0300

    making the navigation responsive

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.querySelector(".fa-bars").addEventListener('click', displayMobileMenu)
+const hiddenNavItems = document.querySelectorAll(".noDisplay")
+
+function displayMobileMenu() {
+    document.querySelector("#menu").classList.add("mobMenu")
+    hiddenNavItems.forEach((navItem) => {
+        navItem.classList.remove("noDisplay")
+    })
+}
+
+document.querySelector(".fa-xmark").addEventListener('click', removeMobileMenu)
+function removeMobileMenu() {
+    hiddenNavItems.forEach((navItem) => {
+        navItem.classList.add("noDisplay")
+    })
+    document.querySelector("#menu").classList.toggle("mobMenu")
+}
