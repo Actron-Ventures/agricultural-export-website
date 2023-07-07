82674581b92cac397b8a5cda0e8c2215eca1c17c
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-07-07 16:37:21 +0300

    mailing

--- contact.php
+++ contact.php
@@ -5,4 +5,33 @@ $number = $_POST['phoneNumber']
 $email = $_POST['email']
 $message = $_POST['message']
 
+$recipient = 'info@beautexhealthcare.com'
+
+$subject = "New Inquiry from" .$name.
+
+$emailBody = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Name: </th>
+<th>Email: </th>
+<th>Phone Number: </th>
+</tr>
+<tr>
+<td>.$name.</td>
+<td>.$email.</td>
+<td>.$number.</td>
+</tr>
+</table>
+</body>
+</html>
+"
+
+mail($recipient, $subject, $emailBody)
+
 ?>
